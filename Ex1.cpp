0f7591455f009cd79f5e53156e0893e5717af519
Evan Labastire <evanlabastire@gmail.com>
2023-09-22 16:54:23 +0200

    first commit

---
+++ Ex1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int *tableau, taille;
+
+    cout << "Combien de valeur voulez vous entrez ?" << endl;
+    cin >> taille;
+    tableau = new int [taille];
+
+    
+}
