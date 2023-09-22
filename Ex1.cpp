1706c48241f1d8b3500e317fec270a77970fd15e
Evan Labastire <evanlabastire@gmail.com>
2023-09-22 17:11:30 +0200

    Ex1

--- Ex1.cpp
+++ Ex1.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 int main()
 {
-    int *tableau, taille;
+    float *tableau, moyenne = 0;
+    int taille;
 
     cout << "Combien de valeur voulez vous entrez ?" << endl;
     cin >> taille;
-    tableau = new int [taille];
+    tableau = new float [taille];
 
+    for (int i = 0; i < taille; i++)
+    {
+        cout << "Donner la valeur n" << i+1 << endl;
+        cin >> tableau[i]; 
+    }
+
+    for (int i = 0; i < taille; i++)
+    {
+        moyenne = moyenne + tableau[i];
+    }
     
+    moyenne = moyenne / taille;
+    cout << "La moyenne est de : " << moyenne << endl;
 }
